df5ac59f1fb39eb1eaba332eaba6930216f99bdd
Pragatheeswaran-sv <pragatheeswaran.s@mitrahsoft.in>
2026-04-24 20:59:29 +0530

    masked_apikey

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -3,9 +3,13 @@ import zipfile
 import pikepdf
 from dotenv import load_dotenv
 import logging
+import cryptography.fernet as fernet
 
 load_dotenv()
 logger = logging.getLogger(__name__)
+
+SECRET_KEY = os.getenv("SECRET_KEY").encode()
+cipher_suite = fernet.Fernet(SECRET_KEY)
 
 def compress_pdf(input_path, output_path):
     try:
@@ -55,7 +59,23 @@ def compress_file(input_file):
         return("Unsupported file type. Only PDF and DOCX allowed.")
 
 
-# 🔹 Example usage
-# if __name__ == "__main__":
-#     file_path = "your_file.pdf"   # or "your_file.docx"
-#     compress_file(file_path)
+def encrypt_data(data):
+    encrypted_value = cipher_suite.encrypt(data.encode()).decode()
+    return encrypted_value
+
+def decrypt_data(encrypted_value):
+    if not encrypted_value:
+        return None
+
+    try:
+        if isinstance(encrypted_value, str):
+            encrypted_value = encrypted_value.encode()
+
+        return cipher_suite.decrypt(encrypted_value).decode()
+
+    except fernet.InvalidToken:
+        # Not a valid encrypted value (plain text or corrupted)
+        return encrypted_value  # return as-is
+
+    except Exception:
+        return None
