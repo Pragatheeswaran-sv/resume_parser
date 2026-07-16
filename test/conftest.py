963c7c468c85c10ec4830a44cdcbe30b79e99ab7
Pragatheeswaran-sv <pragatheeswaran.s@mitrahsoft.in>
2026-07-16 13:41:48 +0530

    add fake secret_key in yml and conftest file

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,4 @@
 import os
 os.environ["DATABASE_URL"] = "sqlite:///:memory:"
 os.environ["JWT_SECRET_KEY"] = "test-secret-key"
-os.environ["SECRET_KEY"] = "dGVzdC1mZXJuZXQta2V5LWZvci1jaS11c2Utb25seQ=="
+os.environ["SECRET_KEY"] = "lw2NJ2Eedo0F8v3Z1hjqH3FZDnwMuF4fbYGW65l1P9k="
